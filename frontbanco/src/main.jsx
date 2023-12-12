de35732c04446f9087587f0e6a321c850e000ba4
joao <joaovmiguez@gmail.com>
2023-12-12 14:13:02 -0300

    Definindo minhas duas primeiras páginas

--- frontbanco/src/main.jsx
+++ frontbanco/src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//1- configurando router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from "./router/pages/homePage/Home";
+import Form from "./router/pages/formCadastro/FormCadastro";
+
+const router = createBrowserRouter([
+
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: 'form',
+    element: <Form />
+  }
+
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
