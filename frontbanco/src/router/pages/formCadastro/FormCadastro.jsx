aafce74bbefbc1acdd35de6bb4b5bc96ed18812d
joao <joaovmiguez@gmail.com>
2023-12-12 14:18:03 -0300

    Criando formulário de cadastro

--- frontbanco/src/router/pages/formCadastro/FormCadastro.jsx
+++ frontbanco/src/router/pages/formCadastro/FormCadastro.jsx
@@ -1,5 +1,86 @@
-export default function Form(){
-    return(
-        <h1>Form</h1>
-    );
+import './formCadastro.css';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+
+
+function Servico() {
+
+  const [atributosCliente, setAtributosCliente] = useState({
+    nome: '',
+    sobrenome:'',
+    cpfCnpj: '',
+    email: '',
+    senha: '',
+    tipoCliente: ''
+  });
+  const [listaAtributos, setListaAtributos] = useState([]);
+
+  function handleChange(event){
+    setAtributosCliente({...atributosCliente, [event.target.name]: event.target.value});
+  }
+
+  function HandleSubmit(event){
+    event.preventDefault();
+    axios.post("http://localhost:8080/clientes", atributosCliente).then(resultado =>{
+      console.log(resultado)
+    })
+  }
+
+  return (
+    <div className="container">
+      
+      <form onSubmit={HandleSubmit}>
+        <h1>Cadastro de cliente</h1>
+
+        <div className='colunas'>
+
+          <div>
+
+              <label className='form-label'>Nome: </label>
+              <input onChange={handleChange} value={atributosCliente.nome} name='nome' type='text' className='form-control'/>
+
+          </div>
+          <div>
+
+              <label className='form-label'>Sobrenome: </label>
+              <input onChange={handleChange} value={atributosCliente.sobrenome} name='sobrenome' type='text' className='form-control'/>
+
+          </div>
+          <div>
+
+              <label className='form-label'>Cpf/Cnpj: </label>
+              <input onChange={handleChange} value={atributosCliente.cpfCnpj} name='cpfCnpj' type='text' className='form-control'/>
+
+          </div>
+          <div>
+
+              <label className='form-label'>Email:</label>
+              <input onChange={handleChange} value={atributosCliente.email}  name='email' type='text' className='form-control'/>
+
+          </div>
+          <div>
+
+              <label className='form-label'>Senha: </label>
+              <input onChange={handleChange} value={atributosCliente.senha}  name='senha' type='password' className='form-control'/>
+
+          </div>
+          <div>
+
+              <label className='form-label'>Tipo do cliente:</label>
+              <input onChange={handleChange} value={atributosCliente.tipoCliente}  name='tipoCliente' type='text' className='form-control'/>
+
+          </div>
+
+          <div className="but">
+            <input className='btn btn-success' type="submit" value='cadastrar' />
+          </div>
+
+
+        </div>
+
+      </form>
+    </div>
+  );
 }
+
+export default Servico;
